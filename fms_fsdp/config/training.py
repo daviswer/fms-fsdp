fc869d883b740509cd4a9a4517a84fd7610224b0
Davis Wertheimer <dww78@cornell.edu>
2024-03-05 20:54:28 -0500

    Update training scheme for 1B

--- fms_fsdp/config/training.py
+++ fms_fsdp/config/training.py
@@ -5,36 +5,36 @@ from dataclasses import dataclass
 class train_config:
     # model
     model_variant: str = "7b"
-    ckpt_load_path: str = "/lustre/pretrain/ckpt"
-    ckpt_save_path: str = "/lustre/pretrain/ckpt"
+    ckpt_load_path: str = "/lustre/dwertheimer/sandbox/ckpt"
+    ckpt_save_path: str = "/lustre/dwertheimer/sandbox/ckpt"
 
     # dataset and dataloader
     use_dummy_dataset: bool = False
-    data_path: str = "/lustre/data"
+    data_path: str = "/lustre/bluepile-processing/rel0_7/tokens/llama2/high_quality_rerun_fuzzy_deduped"
     seq_length: int = 4096
     sep_token: int = 1
-    datasets: str = "lang=en/dataset=commoncrawl,lang=en/dataset=webhose,lang=en/dataset=github_clean,lang=de/dataset=wikipedia,lang=es/dataset=wikipedia,lang=fr/dataset=wikipedia,lang=ja/dataset=wikipedia,lang=pt/dataset=wikipedia,lang=en/dataset=wikimedia,lang=en/dataset=uspto,lang=en/dataset=pubmedcentral,lang=en/dataset=arxiv,lang=en/dataset=stackexchange,lang=en/dataset=PG19"
-    weights: str = "7700,500,550,28,17,22,25,8,100,500,175,250,100,25"
-    logical_shards: int = 768
+    datasets: str = "lang=en/dataset=commoncrawl,"  # lang=en/dataset=webhose,lang=en/dataset=github_clean,lang=de/dataset=wikipedia,lang=es/dataset=wikipedia,lang=fr/dataset=wikipedia,lang=ja/dataset=wikipedia,lang=pt/dataset=wikipedia,lang=en/dataset=wikimedia,lang=en/dataset=uspto,lang=en/dataset=pubmedcentral,lang=en/dataset=arxiv,lang=en/dataset=stackexchange,lang=en/dataset=PG19"
+    weights: str = "1,"  # 7700,500,550,28,17,22,25,8,100,500,175,250,100,25"
+    logical_shards: int = 16
 
     # fsdp policies
     mixed_precision: bool = True
-    fsdp_activation_checkpointing: bool = False
+    fsdp_activation_checkpointing: bool = True
     selective_checkpointing: int = 1
     sharding_strategy: str = "hsdp"
     low_cpu_fsdp: bool = False
 
     # training spec
     seed: int = 2023
-    batch_size: int = 2
-    num_steps: int = 2000000
+    batch_size: int = 32
+    num_steps: int = 20000
     learning_rate: float = 3e-4
     grad_clip_thresh: float = 1.0
 
     # profiling and reporting
     use_profiler: bool = False
-    report_interval: int = 200
-    checkpoint_interval: int = 20000
+    report_interval: int = 20
+    checkpoint_interval: int = 40000
 
     # compile
     use_torch_compile: bool = False
