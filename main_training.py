31f927e9586951d3a5f6f1ddcd2bbe59fd49bf4c
Davis Wertheimer <dww78@cornell.edu>
2024-06-25 13:33:07 -0400

    Skip loading optimizer

--- main_training.py
+++ main_training.py
@@ -118,9 +118,9 @@ def main(**kwargs):
     checkpointer = Checkpointer(
         cfg.ckpt_save_path, 1000, cfg.sharding_strategy, rank, local_rank
     )
-    model, optimizer, _, start_step, tokens_seen = checkpointer.load(
+    model, _, _, start_step, tokens_seen = checkpointer.load(
         model,
-        optimizer,
+        None,
         None,
         path=os.path.join(cfg.ckpt_load_path, "checkpoints/") if not os.path.isfile(cfg.ckpt_load_path) else cfg.ckpt_load_path,
         strict=False,
