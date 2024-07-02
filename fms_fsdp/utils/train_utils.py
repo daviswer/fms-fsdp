f4278ec4b430416c807872f85f2a1897ea842841
Davis Wertheimer <davis.wertheimer@ibm.com>
2024-07-02 17:37:30 -0400

    Remove checkpointing

--- fms_fsdp/utils/train_utils.py
+++ fms_fsdp/utils/train_utils.py
@@ -312,15 +312,6 @@ def train(
             ddp_stats.zero_()
         torch.cuda.reset_peak_memory_stats(device=torch.cuda.current_device())
 
-        if batch_idx % cfg.checkpoint_interval == 0:
-            checkpointer.save(
-                batch_idx,
-                model,
-                optimizer,
-                None,
-                tokens_seen=tokens_seen + new_tokens_seen,
-            )
-
     return train_loss
 
 
