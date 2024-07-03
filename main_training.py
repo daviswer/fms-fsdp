46a4d8da1ac4184432b09edf390572a1b0f94ff3
Davis Wertheimer <dww78@cornell.edu>
2024-07-03 19:16:47 -0400

    Optimizer on merge-mlps only

--- main_training.py
+++ main_training.py
@@ -111,7 +111,7 @@ def main(**kwargs):
 
     # Optimizer
     optimizer = optim.AdamW(
-        model.parameters(), lr=cfg.learning_rate, betas=(0.9, 0.95), weight_decay=0.1
+        [x.attn.w.parameters() for x in model.layers], lr=cfg.learning_rate, betas=(0.9, 0.95), weight_decay=0.1
     )
 
     # optionally load from checkpoint (when continue pretraining)
