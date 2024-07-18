1c2fb20af27ee3cf591d38fa3253ad99095b0701
Davis Wertheimer <davis.wertheimer@ibm.com>
2024-07-18 16:55:55 -0400

    Correct save path

--- main_training.py
+++ main_training.py
@@ -73,7 +73,7 @@ def main(**kwargs):
                 print("step:", batch_idx)
 
         if batch_idx % cfg.checkpoint_interval == 0:
-            with open(cfg.ckpt_save_path+str(batch_idx)+".json", 'w') as f:
+            with open(cfg.ckpt_save_path+"/"+str(batch_idx)+".json", 'w') as f:
                 json.dump({"prompts":prompts}, f)
 
     dist.barrier()
