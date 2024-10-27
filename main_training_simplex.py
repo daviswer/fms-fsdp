9c7209bc8ed66edcb7641195c4376c8767a432f3
Davis Wertheimer <davis.wertheimer@ibm.com>
2024-10-26 22:28:06 -0400

    Custom init

--- main_training_simplex.py
+++ main_training_simplex.py
@@ -281,9 +281,10 @@ def main(**kwargs):
     # Assemble initial simplex and evaluate
     report("ASSEMBLING INITIAL SIMPLEX")
     n = len(mup_params)
-    init_generator = torch.Generator()
-    init_generator.manual_seed(cfg.seed)
-    flips = torch.randn(len(mup_params), generator=init_generator).sign()
+    # init_generator = torch.Generator()
+    # init_generator.manual_seed(cfg.seed)
+    # flips = torch.randn(len(mup_params), generator=init_generator).sign()
+    flips = torch.tensor([-1, 1, -1, 1, 1, -1])
     simplex = torch.eye(n)
     simplex = torch.cat([torch.ones(n, 1).neg().mul(((1+n)**.5-1)/n), simplex], dim=1)
     simplex = simplex - simplex.mean(1, True)
