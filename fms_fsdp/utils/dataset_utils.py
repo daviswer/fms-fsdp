1edeff7660a694ba64f432a727faf0cd109eedf9
Davis Wertheimer <davis.wertheimer@ibm.com>
2024-11-08 13:18:10 -0500

    Diag print

--- fms_fsdp/utils/dataset_utils.py
+++ fms_fsdp/utils/dataset_utils.py
@@ -1439,7 +1439,7 @@ class LoaderMonitor():
             elif deltas[0] > vs[0]:
                 state[k] = torch.nn.functional.pad(state[k], [0,0]*(len(deltas)-1) + [0,deltas[0]-vs[0]])
             # Apply deltas
-            print(k, delta[k][2][0], delta[k][2].dtype, state[k].dtype)
+            assert delta[k][2].dtype==state[k].dtype, (k, delta[k][2][0], state[k][0])
             for i,tup in enumerate(delta[k][1]):
                 state[k][tup.split(1)] = delta[k][2][i]
         return state
