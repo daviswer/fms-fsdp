35eebd5b003cbf63e45eb80404d0742659583576
Davis Wertheimer <davis.wertheimer@ibm.com>
2024-11-19 18:36:33 -0500

    Diag print

--- fms_fsdp/utils/dataset_utils.py
+++ fms_fsdp/utils/dataset_utils.py
@@ -1522,7 +1522,9 @@ def __pop_dstate(state, device_mesh, placements):
     # Flip list[dict[tensor]] to dict[list[tensor]], and concat
     dstate = {k:torch.cat([d[k] for d in dstate], 0) for k in dstate[0]}
     # Construct dtensors from tensors
-    print(v)
+    for k, v in state.items():
+        if not isinstance(v, torch.tensor):
+            print(k,v)
     dstate = {
         k: torch.distributed.tensor.DTensor.from_local(
             v,
