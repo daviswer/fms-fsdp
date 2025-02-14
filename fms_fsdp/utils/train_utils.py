17bd7957d48f289c2e42d6e17b94e794614c303f
Davis Wertheimer <davis.wertheimer@ibm.com>
2025-02-14 17:52:44 -0500

    Explicit past_kv off

--- fms_fsdp/utils/train_utils.py
+++ fms_fsdp/utils/train_utils.py
@@ -85,7 +85,7 @@ def train(
         label = label.to(local_rank)
 
         optimizer.zero_grad()
-        output = model(input, labels=label.long())
+        output = model(input, labels=label.long(), use_cache=False, past_key_values=None)
         # output = output.logits if hasattr(output, "logits") else output
         # ce_loss = torch.nn.CrossEntropyLoss()
         # loss = ce_loss(output.view(-1, output.size(-1)), label.view(-1).long())
