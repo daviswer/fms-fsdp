89b4423e0ec92255e0568561f6e325f4d32aab29
Davis Wertheimer <davis.wertheimer@ibm.com>
2025-02-18 13:21:01 -0500

    Add granite back-conversion

---
+++ fms_to_hf_granite.py
@@ -0,0 +1,41 @@
+import fire
+import torch
+import dolomite_engine.hf_models as hf_models
+from torch.distributed._shard.checkpoint import FileSystemReader, load_state_dict
+
+def main(
+    model_path, compiled, is_old_fms, load_path, save_path, tokenizer_name_or_path
+):
+    print("Initializing model...")
+    model = hf_models.MoEDolomiteForCausalLM.from_pretrained(model_path, device_map="cpu")
+
+    print(f"Reading state dict from {load_path}")
+    if not compiled:
+        state_dict = {"model_state": model.state_dict()}
+    else:
+        state_dict = {"model_state": {"_orig_mod": model.state_dict()}}
+    load_state_dict(
+        state_dict=state_dict, storage_reader=FileSystemReader(load_path), no_dist=True
+    )
+
+    print("Loading state dict into the model...")
+    if not compiled:
+        model.load_state_dict(state_dict["model_state"])
+    else:
+        model.load_state_dict(state_dict["model_state"]["_orig_mod"])
+
+    print("Converting to HF model..")
+    # hf_model = convert_to_hf(model, model_variant, is_old_fms)
+    model.save_pretrained(save_path)
+
+    print("Copying tokenizer...")
+    from transformers import AutoTokenizer
+
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    tokenizer.save_pretrained(save_path)
+
+    print(f"Model converted to HF model, saving at {save_path}")
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
