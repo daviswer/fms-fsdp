96c3be0f51d283b57d5dc26590d443c3fdf17b6c
Davis Wertheimer <dww78@cornell.edu>
2025-02-28 17:29:42 -0500

    Diag save

--- fms_fsdp/utils/train_utils.py
+++ fms_fsdp/utils/train_utils.py
@@ -87,6 +87,8 @@ def train(
         optimizer.zero_grad()
         output = model(input)
         output = output.logits if hasattr(output, "logits") else output
+        if batch_idx==0 and rank==0:
+            torch.save(output.detach().cpu(), os.path.join(cfg.ckpt_load_path, "logits.pth"))
         ce_loss = torch.nn.CrossEntropyLoss()
         loss = ce_loss(output.view(-1, output.size(-1)), label.view(-1).long())
         loss = loss + .0001 * torch.logsumexp(output, dim=-1).pow(2).mean()
