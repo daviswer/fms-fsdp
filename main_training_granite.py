5a1cc1fdc8c11cf9713f3a693277f4b84ae1b686
Linsong Chu <lchu@us.ibm.com>
2025-03-10 15:55:10 -0400

    up

--- main_training_granite.py
+++ main_training_granite.py
@@ -93,8 +93,7 @@ def main(**kwargs):
         use_orig_params=cfg.use_torch_compile,
         device_id=torch.cuda.current_device(),
         limit_all_gathers=True,
-        sync_module_states=True,
-        param_init_fn=param_init_fn,
+        param_init_fn=None,
     )
     if rank == 0:
         print(model)
