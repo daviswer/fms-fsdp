124109bbc43b585f43fd07274fb84621bfafc15a
Davis Wertheimer <dww78@cornell.edu>
2025-04-18 17:32:27 -0400

    Fix conversion

--- fms_fsdp/utils/checkpointing_utils.py
+++ fms_fsdp/utils/checkpointing_utils.py
@@ -353,7 +353,7 @@ def convert_state_dict_to_mamba_ssm(model):
     state_dict = {}
 
     for orig_k in list(original_sd.keys()):
-        k = orig_k.replace("model", "backbone")
+        # k = orig_k.replace("model", "backbone")
         k = k.replace("embed_tokens", "embedding")
         k = k.replace("mamba", "mixer")
         k = k.replace("final_layernorm", "norm_f")
@@ -362,14 +362,15 @@ def convert_state_dict_to_mamba_ssm(model):
         k = k.replace("feed_forward.down_proj", "mlp.fc2")
         k = k.replace("self_attn.o_proj", "mixer.out_proj")
         if k != orig_k:
-            state_dict[k] = original_sd.pop(orig_k)
+            state_dict["backbone."+k] = original_sd.pop(orig_k)
     for i in range(len(model.layers)):
         w1 = original_sd.pop(f"layers.{i}.feed_forward.up_proj.weight")
         w2 = original_sd.pop(f"layers.{i}.feed_forward.gate_proj.weight")
-        state_dict[f"layers.{i}.mlp.fc1.weight"] = torch.cat([w1,w2], dim=0)
+        state_dict[f"backbone.layers.{i}.mlp.fc1.weight"] = torch.cat([w1,w2], dim=0)
         if f"layers.{i}.self_attn.q_proj.weight" in original_sd:
             q = original_sd.pop(f"layers.{i}.self_attn.q_proj.weight")
             k = original_sd.pop(f"layers.{i}.self_attn.k_proj.weight")
             v = original_sd.pop(f"layers.{i}.self_attn.v_proj.weight")
-            state_dict[f"layers.{i}.mixer.in_proj.weight"] = torch.cat([q,k,v], dim=0)
-    return state_dict, original_sd
+            state_dict[f"backbone.layers.{i}.mixer.in_proj.weight"] = torch.cat([q,k,v], dim=0)
+    assert len(original_sd==0), original_sd.keys()
+    return {"model_state":state_dict}
