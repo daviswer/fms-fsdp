d65c1d45a31f2430a8eb31d2d7617f6ef1c0f4db
Davis Wertheimer <dww78@cornell.edu>
2025-05-08 16:42:35 -0400

    Diag log

--- main_training_llama.py
+++ main_training_llama.py
@@ -2,6 +2,7 @@ import math
 import os
 
 import fire
+import logging
 import torch
 import torch.optim as optim
 from fms.models.llama import LLaMA, LLaMABlock
@@ -23,6 +24,9 @@ from fms_fsdp.utils.train_utils import (
 
 
 def main(**kwargs):
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.INFO)
+    
     # get configs
     cfg = config.train_config()
     update_config(cfg, **kwargs)
