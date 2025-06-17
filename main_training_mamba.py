8ae658357d59a7bc9d74d6f2907f1c29b9701a54
Davis Wertheimer <dww78@cornell.edu>
2025-06-17 14:05:26 -0400

    zlwd (#18)
    * make mamba
    
    * add quick debug
    
    * add quick debug
    
    * revert debug verbosity
    
    * Learning rate scheduler changed (Constant)
    
    * Add AutoHandler
    
    * Add Auto cfg option for AutoHAndler
    
    * Len gets called before open
    
    * path/filepath typo fix
    
    * Partitioning fix from mup-search
    
    * Cosine 0.01 decay
    
    * Warmup interval change
    
    * Schedule change
    
    * Constant schedule
    
    * LR schedule change (cool down and constant lr)
    
    * Update dataset_utils.py
    
    Added a check for length of doc
    
    * LR schedule change (Warmup + constant)
    
    * Update dataset_utils.py
    
    * Cosine schedule
    
    * For constant lr 1.5e5
    
    * Schedule change
    
    * Schedule change
    
    * Added cool down
    
    * length of doc check
    
    * splitstrip cols and pass to fhandler
    
    * fhandler col_names support
    
    * Warmup for annealing
    
    * Debugging
    
    * Debugging II
    
    * Empty shard check
    
    * Added constant lr schedule with warmup
    
    * added print for lenght of doc
    
    * added print for lenght of doc II
    
    * Update dataset_utils.py
    
    * Add partial wd
    
    * Add zl
    
    * Hardcode orig_params True
    
    * Turn off rope
    
    ---------
    
    Co-authored-by: Linsong Chu <lchu@us.ibm.com>
    Co-authored-by: divykum2 <divykum2@ibm.com>
    Co-authored-by: divya-kumari32 <72085811+divya-kumari32@users.noreply.github.com>

--- main_training_mamba.py
+++ main_training_mamba.py
@@ -86,7 +86,7 @@ def main(**kwargs):
         auto_wrap_policy=wrapping_policy,
         mixed_precision=mixed_precision_policy,
         sharding_strategy=sharding_strategy_policy,
-        use_orig_params=cfg.use_torch_compile,
+        use_orig_params=True,
         device_id=torch.cuda.current_device(),
         limit_all_gathers=True,
         param_init_fn=param_init_fn,
@@ -107,8 +107,35 @@ def main(**kwargs):
         model = torch.compile(model)
 
     # Optimizer
+    # optimizer = optim.AdamW(
+    #     model.parameters(), lr=cfg.learning_rate, betas=(0.9, 0.95), weight_decay=0.1
+    # )
+    params_with_decay = []
+    params_without_decay = []
+    for name, param in model.named_parameters():
+        print(f'{name=}')
+        if 'A_log' in name or 'D' in name or 'dt_bias' in name:
+            params_without_decay.append(param)
+        else:
+            params_with_decay.append(param)
+
+
+    print(f'{params_with_decay=}')
+    print(f'{params_without_decay=}')
+
     optimizer = optim.AdamW(
-        model.parameters(), lr=cfg.learning_rate, betas=(0.9, 0.95), weight_decay=0.1
+        [
+            {
+                "params": params_with_decay,
+                "weight_decay": 0.1,
+            },
+            {
+                "params": params_without_decay,
+                "weight_decay": 0.,
+            },
+        ],
+        betas = (0.9, 0.95),
+        lr = cfg.learning_rate, # cfg.learning_rate,
     )
 
     # optionally load from checkpoint (when continue pretraining)
@@ -135,8 +162,9 @@ def main(**kwargs):
     # LR schedule
     # linear decay for annealing
     if cfg.training_stage == "annealing":
-        schedule = lambda x: 1 - x / cfg.num_steps
-    else:
+        warmup_interval = 1000
+        schedule = lambda x: x / warmup_interval if x < warmup_interval else 1 - (x - warmup_interval) / (cfg.num_steps - warmup_interval)
+    elif cfg.training_stage == "cosine":
         # cosine decay
         warmup_interval = min(2000, cfg.num_steps // 20)
         schedule = lambda x: min(
@@ -146,6 +174,11 @@ def main(**kwargs):
             * (1 - 0.1)
             * (1 + math.cos(min(x, cfg.num_steps) / cfg.num_steps * math.pi)),
         )
+    elif cfg.training_stage == "constant":
+        warmup_interval = 2000
+        schedule = lambda x: (min(x, warmup_interval) / warmup_interval)
+    else:
+        schedule = lambda x: 1.0 + (0.75 - 1.0) * (x / 32000) if x <= 32000 else 0.75
 
     scheduler = LambdaLR(optimizer, lambda x: schedule(x + start_step))
 
