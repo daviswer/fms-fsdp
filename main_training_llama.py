2b5ac876715c6cff918d087277b068583b38281d
Davis Wertheimer <dww78@cornell.edu>
2025-08-06 15:10:01 -0400

    Save to cpu correctly

--- main_training_llama.py
+++ main_training_llama.py
@@ -210,10 +210,10 @@ def main(**kwargs):
     next_tok = preds[:,-1].argmax(-1)
     print(next_tok, t.decode(next_tok))
 
-    cache_to_save = [[t[-10:] for t in l] for l in cache]
+    cache_to_save = [[t[-10:].cpu() for t in l] for l in cache]
     
     if rank == 0:
-        torch.save(cache_to_save.cpu(), "/gpfs/davis/ua_sigtest_cache.pth")
+        torch.save(cache_to_save, "/gpfs/davis/ua_sigtest_cache.pth")
     
 
     dist.barrier()
