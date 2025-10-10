c96a6c468514451d3b9d18dfe492da9cd67812c5
cliu-us <cliu@us.ibm.com>
2025-10-10 13:00:04 -0400

    yoco default cfg fix
    Signed-off-by: cliu-us <cliu@us.ibm.com>

---
+++ yoco_fwd_test.py
@@ -0,0 +1,15 @@
+from fms_fsdp.utils.config_utils import get_model_config
+from fms.models.llama_yoco import LLaMA
+from transformers import AutoTokenizer
+
+llama_config = get_model_config("llama_1b")
+model = LLaMA(llama_config)
+model.half().cuda()
+
+print(model)
+
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
+input_ids = tokenizer("What is your favorite TV show?", return_tensors="pt").input_ids.to(next(model.parameters()).device)
+output = model(input_ids)
+
+print(output)
