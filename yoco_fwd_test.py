b41798bf61c46952d592fdfdd644400bb0c304ad
cliu-us <cliu@us.ibm.com>
2025-10-16 10:03:26 -0400

    vscode debug
    Signed-off-by: cliu-us <cliu@us.ibm.com>

--- yoco_fwd_test.py
+++ yoco_fwd_test.py
@@ -1,15 +1,20 @@
 from fms_fsdp.utils.config_utils import get_model_config
 from fms.models.llama_yoco import LLaMA
 from transformers import AutoTokenizer
+import torch
 
 llama_config = get_model_config("llama_1b")
 model = LLaMA(llama_config)
+model.reset_parameters()
 model.half().cuda()
 
 print(model)
 
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
 input_ids = tokenizer("What is your favorite TV show?", return_tensors="pt").input_ids.to(next(model.parameters()).device)
-output = model(input_ids)
+
+with torch.no_grad():
+    output = model(input_ids)
 
 print(output)
+
