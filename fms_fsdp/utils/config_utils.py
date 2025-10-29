351d77e916d4d36a5f9e0677a2145ac9cb904655
Davis Wertheimer <dww78@cornell.edu>
2025-10-29 16:22:47 -0400

    Cleanup, add incremental triton cfg

--- fms_fsdp/utils/config_utils.py
+++ fms_fsdp/utils/config_utils.py
@@ -299,6 +299,17 @@ def get_model_config(model_variant):
             max_expected_seq_len=4096,
             rope_theta=500000.0,
         )
+    elif model_variant == "llama_1b_triton":
+        model_config = LLaMAConfig(
+            src_vocab_size=128256,
+            emb_dim=1280,
+            nheads=20,
+            kvheads=5,
+            nlayers=32,
+            hidden_grow_factor=3.2,
+            max_expected_seq_len=4096,
+            rope_theta=500000.0,
+        )
     elif model_variant == "llama_1b_gated":
         model_config = LLaMAConfig(
             src_vocab_size=128256,
@@ -379,19 +390,6 @@ def get_model_config(model_variant):
             kvheads=5,
             nlayers=32,
             hidden_grow_factor=3.2,
-            max_expected_seq_len=4096,
-            rope_theta=32,
-            rope_scaling={"rope_type":"unrope"},
-            rope_partial=.5,
-        )
-    elif model_variant == "llama_1b_unrope32_triton2":
-        model_config = LLaMAConfig(
-            src_vocab_size=128256,
-            emb_dim=1024,
-            nheads=16,
-            kvheads=4,
-            nlayers=32,
-            hidden_grow_factor=3.5,
             max_expected_seq_len=4096,
             rope_theta=32,
             rope_scaling={"rope_type":"unrope"},
