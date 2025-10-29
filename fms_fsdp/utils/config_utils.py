c538e2e9e4a5c18e767d60e71c0e9c6f2e9ae764
Davis Wertheimer <dww78@cornell.edu>
2025-10-29 17:27:25 -0400

    Clear triton, just go gated

--- fms_fsdp/utils/config_utils.py
+++ fms_fsdp/utils/config_utils.py
@@ -299,17 +299,6 @@ def get_model_config(model_variant):
             max_expected_seq_len=4096,
             rope_theta=500000.0,
         )
-    elif model_variant == "llama_1b_triton":
-        model_config = LLaMAConfig(
-            src_vocab_size=128256,
-            emb_dim=1280,
-            nheads=20,
-            kvheads=5,
-            nlayers=32,
-            hidden_grow_factor=3.2,
-            max_expected_seq_len=4096,
-            rope_theta=500000.0,
-        )
     elif model_variant == "llama_1b_gated":
         model_config = LLaMAConfig(
             src_vocab_size=128256,
@@ -317,7 +306,7 @@ def get_model_config(model_variant):
             nheads=16,
             kvheads=4,
             nlayers=32,
-            hidden_grow_factor=3,
+            hidden_grow_factor=2.4,
             max_expected_seq_len=4096,
             rope_theta=500000.0,
         )
