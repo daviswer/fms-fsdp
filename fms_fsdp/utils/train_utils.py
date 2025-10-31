31e47be83008349c1973608d84da5b334612ef0d
Davis Wertheimer <davis.wertheimer@ibm.com>
2025-10-31 14:46:08 -0400

    Diag barrier

--- fms_fsdp/utils/train_utils.py
+++ fms_fsdp/utils/train_utils.py
@@ -92,6 +92,7 @@ def train(
             if rank==0:
                 print(f".   Cache retrieved. Len is {len(cache)}, sizes are {cache[0][0].shape} and {cache[-1][0].shape}")
             pred = samples
+            dist.barrier()
             for i in range(10):
                 pred, _ = model(pred, prompt)
                 print(f".   Step {i} pred: {pred}")
