d00e354b2a5fc05191ad6266d591cbfdd2aca7ee
Davis Wertheimer <dww78@cornell.edu>
2025-12-03 14:54:19 -0500

    Pass aux_sparsity flag

--- main_training_llama.py
+++ main_training_llama.py
@@ -60,6 +60,7 @@ def main(**kwargs):
 
     # get fms model
     llama_config = get_model_config(cfg.model_variant)
+    llama_config = cfg.aux_sparsity
     if cfg.low_cpu_fsdp:
         with torch.device("meta"):
             model = LLaMA(llama_config)
