97c4b6901d8bc1215004694897262000cab12f8b
Davis Wertheimer <davis.wertheimer@ibm.com>
2025-12-12 00:34:59 -0500

    Equivalence check for datagen pt2

--- fms_fsdp/utils/train_utils.py
+++ fms_fsdp/utils/train_utils.py
@@ -132,8 +132,8 @@ def train(
                 past_key_value_states = dec_cache,
             )
             if local_rank==0:
-                print(flowovers[4][0][[0,128,256]])
-                print(output[ids[0]][[0,128,256]].argmax(-1))
+                print(flowovers[4][:,0])
+                print(output[ids][:,0].argmax(-1))
 
         if profiler:
             profiler.step()
