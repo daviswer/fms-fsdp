e07e51064f21f1d61650a0f5406e86592f3eab37
Davis Wertheimer <davis.wertheimer@ibm.com>
2025-12-12 00:40:40 -0500

    Lock for test

--- fms_fsdp/utils/train_utils.py
+++ fms_fsdp/utils/train_utils.py
@@ -118,6 +118,7 @@ def train(
         # Generate fresh flowover corruption data
         with torch.no_grad():
             ids = torch.randperm(history.size(0)).to(local_rank)[:history.size(0)//flowover_denom]
+            ids[0] = 0  # TODO: remove after equiv testing
             flowovers = [x[ids] for x in [history, ground_truth, dec_input, dec_history, corruption]]
             embeds = embeds[ids]
             dec_cache[0][0] = dec_cache[0][0][ids]
