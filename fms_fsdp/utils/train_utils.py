d15ef2d38837317b360ab1285c8c1cd57959fda7
Davis Wertheimer <dww78@cornell.edu>
2026-01-05 15:03:50 -0500

    Use save path directly

--- fms_fsdp/utils/train_utils.py
+++ fms_fsdp/utils/train_utils.py
@@ -87,7 +87,7 @@ def train(
         optimizer.zero_grad()
         output, masks = model(input)
         if rank==0:
-            torch.save([input.cpu(),masks], os.path.join(cfg.ckpt_save_path, "masks.pth"))
+            torch.save([input.cpu(),masks], cfg.ckpt_save_path)
         dist.barrier()
         assert False
         
