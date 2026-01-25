13f3d962fe54972bb10ad58f1dcb6c7ccb7d6586
Davis Wertheimer <davis.wertheimer@ibm.com>
2026-01-25 16:50:45 -0500

    Corrected init policy

--- fms_fsdp/policies/param_init.py
+++ fms_fsdp/policies/param_init.py
@@ -3,7 +3,7 @@ from fms.modules.attention import MultiHeadAttention
 from torch.nn import Embedding
 from fms.modules.feedforward import GatedLinearUnit
 from fms.modules.layernorm import LayerNormParameterized
-from fms.modules.head import MLPClassificationHead
+from fms.modules.head import CFGHead
 
 
 # for details, read https://github.com/foundation-model-stack/fms-fsdp/issues/64
@@ -13,10 +13,8 @@ def param_init_function(module):
         or isinstance(module, Embedding)
         or isinstance(module, GatedLinearUnit)
         or isinstance(module, LayerNormParameterized)
-        or isinstance(module, MLPClassificationHead)
+        or isinstance(module, CFGHead)
     ):
-        if isinstance(module, MLPClassificationHead):
-            print("FOUND THE MODULE")
         module.to_empty(device=torch.cuda.current_device())
         with torch.no_grad():
             module.reset_parameters()
