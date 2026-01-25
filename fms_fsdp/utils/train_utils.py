bd8b8c44e78ca915f5f05645ff82928f2ee2ab36
Davis Wertheimer <davis.wertheimer@ibm.com>
2026-01-25 16:34:23 -0500

    Fix diag

--- fms_fsdp/utils/train_utils.py
+++ fms_fsdp/utils/train_utils.py
@@ -87,7 +87,7 @@ def train(
     for batch_idx, (input, label) in enumerate(train_loader, start=start_step + 1):
         if batch_idx > cfg.num_steps:
             break
-        if batch_idx==0:
+        if batch_idx==1:
             print(model.mlp.mlp[3].weight.mean())
         input = input.to(local_rank)
         label = label.to(local_rank)
