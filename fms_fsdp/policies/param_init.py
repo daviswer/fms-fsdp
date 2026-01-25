994f3ca0b22c291863255927704362ae3047f63b
Davis Wertheimer <davis.wertheimer@ibm.com>
2026-01-25 16:57:59 -0500

    Reset linear head too

--- fms_fsdp/policies/param_init.py
+++ fms_fsdp/policies/param_init.py
@@ -3,7 +3,7 @@ from fms.modules.attention import MultiHeadAttention
 from torch.nn import Embedding
 from fms.modules.feedforward import GatedLinearUnit
 from fms.modules.layernorm import LayerNormParameterized
-from fms.modules.head import CFGHead
+from fms.modules.head import CFGHead, LinearClassificationHead
 
 
 # for details, read https://github.com/foundation-model-stack/fms-fsdp/issues/64
@@ -14,6 +14,7 @@ def param_init_function(module):
         or isinstance(module, GatedLinearUnit)
         or isinstance(module, LayerNormParameterized)
         or isinstance(module, CFGHead)
+        or isinstance(module, LinearClassificationHead)
     ):
         module.to_empty(device=torch.cuda.current_device())
         with torch.no_grad():
