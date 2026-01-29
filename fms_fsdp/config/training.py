d29d9d6712fb3def9221e513096f5aeb45596404
Davis Wertheimer <davis.wertheimer@ibm.com>
2026-01-28 20:17:31 -0500

    Refactor denom to prevent overload on m

--- fms_fsdp/config/training.py
+++ fms_fsdp/config/training.py
@@ -50,7 +50,7 @@ class train_config:
     grad_clip_thresh: float = 1.0
     seed: int = 2023
     zl_coeff: float = 1e-4
-    mix_denom: float = 2.0
+    avg_denom: float = 2.0
     mtp_denom: float = 5.0
 
     # continued training spec
