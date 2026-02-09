7cc40e79d8c862089fd8c70fe07eeb2f6c823fd2
Davis Wertheimer <davis.wertheimer@ibm.com>
2026-02-09 13:49:58 -0500

    Full reporting

--- fms_fsdp/utils/train_utils.py
+++ fms_fsdp/utils/train_utils.py
@@ -141,15 +141,13 @@ def train(
                 print("step:", batch_idx)
                 print("loss:", current_loss)
                 print("aux loss:", current_aux)
+                print("thresh:", cfg.prune_thresh)
+                print("pruning:", cfg.prune)
                 print("LR:", current_lr)
                 print("tokens seen:", total_tokens_seen)
                 print("gradient norm:", current_gnorm)
-                print("reserved memory:", reserved_mem)
-                print("allocated memory:", allocated_mem)
                 print("current step time:", current_step_time)
-                print("overall step time:", overall_step_time)
                 print("current token per gpu per sec:", current_throughput)
-                print("overall token per gpu per sec:", overall_throughput)
                 print(
                     "overall token per day:",
                     int(new_tokens_seen / elapsed_time * 3600 * 24),
